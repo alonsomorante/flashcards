ecefafff7990b2148f8c5117a21b5e06a8663931
Alonso <morantealo@hotmail.com>
2026-07-20 20:24:57 -0500

    flashcards new

--- frontend/src/hooks/useSpellCheck.ts
+++ frontend/src/hooks/useSpellCheck.ts
@@ -1,8 +1,7 @@
-import { useMutation } from '@tanstack/react-query';
-import { api } from '../api/client';
-
+import { useMutation } from '@tanstack/react-query'
+import { api } from '../api/client'
 export function useSpellCheck() {
   return useMutation({
     mutationFn: (text: string) => api.spellCheck(text),
-  });
+  })
 }
